87f77a65cacd77aa69610a9d22a3d9cde6ff8257
Vincent <vincentbui2108@gmail.com>
2023-03-14 20:49:53 +0200

    successfully detect object with color inside a square

--- test2.py
+++ test2.py
@@ -14,7 +14,6 @@ while True:
     c=int(h*(1/3))
     d=int(h*(2/3))
 
-    #cap=cv2.line(frame,(0,0), (10,height),(0,0,0),5)
 
     cv2.rectangle(frame,(a,0), (a+c,c),(0,0,0),5)
     cv2.rectangle(frame,(a+c, 0), (a+d,c), (0,0,0), 5)
@@ -27,6 +26,24 @@ while True:
     cv2.rectangle(frame,(a, d), (a+c,h), (0,0,0), 5)
     cv2.rectangle(frame,(a+c, d), (a+d,h), (0,0,0), 5)
     cv2.rectangle(frame,(a+d, d), (a+h,h), (0,0,0), 5)
+
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    lower_blue = np.array([90, 50, 50])
+    upper_blue = np.array([130, 255, 255])
+
+    mask = cv2.inRange(hsv, lower_blue, upper_blue)
+    blur = cv2.GaussianBlur(mask, (5, 5), 0)
+
+    contours, hierarchy = cv2.findContours(blur, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    for contour in contours:
+        area = cv2.contourArea(contour)
+        if area > 100:
+            x, y, w, h = cv2.boundingRect(contour)
+            if x > a and y > 0 and x + w < a+c and y + h < c:
+                print("Blue object found inside the square.")
+                break
 
     cv2.imshow("",frame)
     if cv2.waitKey(1)==ord("q"):
