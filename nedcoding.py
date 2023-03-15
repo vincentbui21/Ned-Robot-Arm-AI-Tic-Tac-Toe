fbeda3f28fcf75a2a7ebd8fa7b880c280053d589
Vincent <vincentbui2108@gmail.com>
2023-03-15 10:52:08 +0200

    create 2 new files (ttt and nedcoding)

---
+++ nedcoding.py
@@ -0,0 +1,12 @@
+from pyniryo import *
+
+robot = NiryoRobot("10.10.10.10")
+robot.calibrate_auto()
+robot.update_tool()
+
+obsevervation_pose = 0,-0.32,0,0,-1.568,0
+
+robot.move_joints(obsevervation_pose) #observation pose
+
+
+robot.close_connection()
---
+++ ttt.py
@@ -0,0 +1,9 @@
+board = [ 
+        [0,0,0],
+        [0,0,0],
+        [0,0,0]
+        ]
+
+#the board variable (2D list) now represents as the tic-tac-toe board.
+#let's all agree: 0_none ; 1_X exist; -1_O exist 
+
