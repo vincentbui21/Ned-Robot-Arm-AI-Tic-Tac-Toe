f0f560b26b2177ae9c65e2c91a3999acfb0b12cf
Vincent <vincentbui2108@gmail.com>
2023-03-16 20:20:59 +0200

    work done with openCV

--- opencv.py
+++ opencv.py
@@ -1,31 +1,37 @@
 import cv2
 import numpy as np
 import time
-from pyniryo import * 
-import ttt
-"""
-robot = NiryoRobot("10.10.10.10")
-robot.calibrate_auto()
-obsevervation_pose = 0,-0.38,-0.08,0,-1.45,0
-robot.move_joints(obsevervation_pose) #observation pose
-"""
+
 
 
 def detect_move():
+    from aicode import board #or aicode
+    
     img=cv2.VideoCapture(0)
+    
+
     arr=[0,0,0,0,0,0,0,0,0]
-    while True:
+    while True:       
         time_count = img.get(cv2.CAP_PROP_FPS) * 3
-        ret,frame = img.read()
-        frame = cv2.flip(frame, 1)
-        #frame = cv2.flip(frame, -1)
+        ret,frame = img.read()      
+        frame = cv2.flip(frame, 1)      
 
         w = int(img.get(3))
         h = int(img.get(4))
+       
         a=int((w-h)/2)
         c=int(h*(1/3))
         d=int(h*(2/3))
 
+        y1=int((1/6)*h)
+        y2=int((1/2)*h)
+        y3=int((5/6)*h)
+
+        x1=int(a + (1/6)*h)
+        x2=int(a + (1/2)*h)
+        x3=int(a + (5/6)*h)
+
+        radius = y1
 
         cv2.rectangle(frame,(a,0), (a+c,c),(0,0,0),5)
         cv2.rectangle(frame,(a+c, 0), (a+d,c), (0,0,0), 5)
@@ -48,10 +54,63 @@ def detect_move():
         blur = cv2.GaussianBlur(mask, (5, 5), 0)
 
         contours, hierarchy = cv2.findContours(blur, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
-        start_time = 0
-        
-
+       
+        for i in range(0,9):
+            match i:
+                case 0:
+                    if board[i] == "X":
+                        cv2.line(frame, (a,0), (a+c,c),(0,255,0), 3)
+                        cv2.line(frame, (a+c,0), (a,c),(0,255,0), 3)
+                    elif board[i]=="O":
+                        cv2.circle(frame,(x1,y1), radius, (0,255,0),3)
+                case 1:
+                    if board[i] == "X":
+                        cv2.line(frame, (a+c,0), (a+d,c),(0,255,0), 3)
+                        cv2.line(frame, (a+d,0), (a+c,c),(0,255,0), 3)
+                    elif board[i]=="O":
+                        cv2.circle(frame,(x2,y1), radius, (0,255,0),3)
+                case 2:
+                    if board[i] == "X":
+                        cv2.line(frame, (a+d,0), (a+h,c),(0,255,0), 3)
+                        cv2.line(frame, (a+h,0), (a+d,c),(0,255,0), 3)
+                    elif board[i]=="O":
+                        cv2.circle(frame,(x3,y1), radius, (0,255,0),3)
+                case 3:
+                    if board[i] == "X":
+                        cv2.line(frame, (a,c), (a+c,d),(0,255,0), 3)
+                        cv2.line(frame, (a+c,c), (a,d),(0,255,0), 3)
+                    elif board[i]=="O":
+                        cv2.circle(frame,(x1,y2), radius, (0,255,0),3)
+                case 4:
+                    if board[i] == "X":
+                        cv2.line(frame, (a+c,c), (a+d,d),(0,255,0), 3)
+                        cv2.line(frame, (a+d,c), (a+c,d),(0,255,0), 3)
+                    elif board[i]=="O":
+                        cv2.circle(frame,(x2,y2), radius, (0,255,0),3)
+                case 5:
+                    if board[i] == "X":
+                        cv2.line(frame, (a+d,c), (a+h,d),(0,255,0), 3)
+                        cv2.line(frame, (a+h,c), (a+d,d),(0,255,0), 3)
+                    elif board[i]=="O":
+                        cv2.circle(frame,(x3,y2), radius, (0,255,0),3)
+                case 6:
+                    if board[i] == "X":
+                        cv2.line(frame, (a,d), (a+c,h),(0,255,0), 3)
+                        cv2.line(frame, (a+c,d), (a,h),(0,255,0), 3)
+                    elif board[i]=="O":
+                        cv2.circle(frame,(x1,y3), radius, (0,255,0),3)
+                case 7:
+                    if board[i] == "X":
+                        cv2.line(frame, (a+d,d), (a+c,h),(0,255,0), 3)
+                        cv2.line(frame, (a+c,d), (a+d,h),(0,255,0), 3)
+                    elif board[i]=="O":
+                        cv2.circle(frame,(x2,y3), radius, (0,255,0),3)
+                case 8:
+                    if board[i] == "X":
+                        cv2.line(frame, (a+d,d), (a+h,h),(0,255,0), 3)
+                        cv2.line(frame, (a+h,d), (a+d,h),(0,255,0), 3)
+                    elif board[i]=="O":
+                        cv2.circle(frame,(x3,y3), radius, (0,255,0),3)
 
         for contour in contours:
             area = cv2.contourArea(contour)
@@ -59,65 +118,65 @@ def detect_move():
                 cv2.drawContours(frame, [contour], -1, (0, 255, 0), 2)
                 x, y, w, hei = cv2.boundingRect(contour)
                 
-                if x > a and y > 0 and x + w < a+c and y + hei < c and ttt.board[0][0]==0: #first square                                               
+                if x > a and y > 0 and x + w < a+c and y + hei < c and board[0]==' ': #first square                                               
                     arr[0]+=1
                     if arr[0]==time_count:
-                        print("Object found inside 1st square")
-                        return 0,0
+                        print("Object found inside 1st square")                      
+                        return 0
                                     
-                elif x > a+c and y > 0 and x + w < a+d and y + hei < c and ttt.board[0][1]==0: #second square
+                elif x > a+c and y > 0 and x + w < a+d and y + hei < c and board[1]==' ': #second square
                     arr[1]+=1
                     if arr[1]==time_count:
                         print("Object found inside 2nd square")
-                        return 0,1
+                        return 1
                                       
-                elif x > a+d and y > 0 and x + w < (a+h) and y + hei < c and ttt.board[0][2]==0: #third square
+                elif x > a+d and y > 0 and x + w < (a+h) and y + hei < c and board[2]==' ': #third square
                     arr[2]+=1
                     if arr[2]==time_count:
                         print("Object found inside 3rd square")
-                        return 0,2
+                        return 2
                             
-                elif x > a and y > c and x + w < a+c and y + hei < d and ttt.board[1][0]==0: #forth square
+                elif x > a and y > c and x + w < a+c and y + hei < d and board[3]==' ': #forth square
                     arr[3]+=1
                     if arr[3]==time_count:
                         print("Object found inside 4th square")
-                        return 1,0
+                        return 3
                                
-                elif x > a+c and y > c and x + w < a+d and y + hei < d and ttt.board[1][1]==0: #fifth square
+                elif x > a+c and y > c and x + w < a+d and y + hei < d and board[4]==' ': #fifth square
                     arr[4]+=1
                     if arr[4]==time_count:
                         print("Object found inside 5th square")
-                        return 1,1
+                        return 4
                             
-                elif x > a+d and y > c and x + w < a+h and y + hei < d and ttt.board[1][2]==0: #sixth square
+                elif x > a+d and y > c and x + w < a+h and y + hei < d and board[5]==' ': #sixth square
                     arr[5]+=1
                     if arr[5]==time_count:
                         print("Object found inside 6th square")
-                        return 1,2
+                        return 5
                                 
-                elif x > a and y > d and x + w < a+c and y + hei < h and ttt.board[2][0]==0: #seventh square
+                elif x > a and y > d and x + w < a+c and y + hei < h and board[6]==' ': #seventh square
                     arr[6]+=1
                     if arr[6]==time_count:
                         print("Object found inside 7th square")
-                        return 2,0
+                        return 6
                                  
-                elif x > a+c and y > d and x + w < a+d and y + hei < h and ttt.board[2][1]==0: #eighth square
+                elif x > a+c and y > d and x + w < a+d and y + hei < h and board[7]==' ': #eighth square
                     arr[7]+=1
                     if arr[7]==time_count:
                         print("Object found inside 8th square")
-                        return 2,1
+                        return 7
                                   
-                elif x > a+d and y > d and x + w < a+h and y + hei < h and ttt.board[2][2]==0: #ninth square
+                elif x > a+d and y > d and x + w < a+h and y + hei < h and board[8]==' ': #ninth square
                     arr[8]+=1
                     if arr[8]==time_count:
                         print("Object found inside 9th square")
-                        return 2,2
+                        return 8
                                  
                 else:
                     pass   
 
-        cv2.imshow("",frame)
+        cv2.imshow("Player turn",frame)
         if cv2.waitKey(1)==ord("q"):
             break           
-    img.release()
+    
 
--- ttt.py
+++ ttt.py
@@ -1,14 +1,14 @@
 from opencv import detect_move
 
+
 board = [ 
-        [0,0,0],
-        [0,0,0],
-        [0,0,0]
+        ' ', ' ',' ',
+        ' ', ' ',' ',
+        ' ', ' ',' '
         ]
 
-#the board variable (2D list) now represents as the tic-tac-toe board.
-#let's all agree: 0_none ; 1_o exist (player); -1_x exist(AI) 
 
-a, b=detect_move()
-board[a][b] = "o"
-print(board[a][b])
+a=detect_move()
+board[a] = "o"
+print(board[a])
+
